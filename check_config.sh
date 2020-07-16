6c140778cc05e261cbcfa71462d9d44e12aac26e
zhangzhixin <972844082@qq.com>
2020-07-16 19:03:23 +0800

    修改.sh文件

--- check_config.sh
+++ check_config.sh
@@ -4,4 +4,4 @@ config_result=`sudo docker-compose logs config`
 
 echo $config_result
 
-[[ $config_result =~ "Config Success!" ]] || exit -1
+[[ $config_result =~ "Config Success!" ]] || exit 1
--- check_poc.sh
+++ check_poc.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 sleep 600
-echo "1111111"
-poc_config=`sudo docker-compose logs config`
 poc_result=`sudo docker-compose logs poc`
-echo "2111111"
-echo $poc_config
 echo $poc_result
 
-[[ $poc_result =~ "Poc Success!" ]] || exit -1
+[[ $poc_result =~ "Poc Success!" ]] || exit 1
