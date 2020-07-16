dc2113cc8455a14a225615b9b2a01ed70d9e34de
zhangzhixin <972844082@qq.com>
2020-07-16 20:43:20 +0800

    4

--- check_poc.sh
+++ check_poc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 600
-poc_result=`sudo docker-compose logs poc`
-echo $poc_result
+poc_result=$(sudo docker-compose logs poc)
+echo "$poc_result"
 
 [[ $poc_result =~ "Poc Success!" ]] || exit 1
