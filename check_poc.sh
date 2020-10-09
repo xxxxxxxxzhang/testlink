7bc9cb4ce06f5fe333b5b0ec7e98ee550658f559
xxxxxxxxzhang <972844082@qq.com>
2020-10-09 13:22:18 +0800

    修改login.py

--- check_poc.sh
+++ check_poc.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-sleep 600
+STATUS=""
+while [ "$STATUS" != "200" ]
+do
+    sleep 3
+	echo "init is not enough, please wait....."
+    STATUS=$(curl -sL -w "%{http_code}" http://127.0.0.1:8000 -o /dev/null)
+    
+done
+sleep 30
 poc_result=$(sudo docker-compose logs poc)
 echo "$poc_result"
 
